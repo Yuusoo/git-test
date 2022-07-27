e8cd9d9a1e217a4ea749b2d542022794c2856877
yuusoo <yusoo@163.com>
2022-07-27 15:21:08 +0800

    second commit

--- src/main/java/yys/hst/git/Gittest.java
+++ src/main/java/yys/hst/git/Gittest.java
@@ -8,5 +8,6 @@ package yys.hst.git;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello.git");
+        System.out.println("hello.git");
     }
 }
