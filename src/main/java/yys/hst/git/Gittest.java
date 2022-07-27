77094e5826aa68b2c1cc6c74fa7ba7272d7fd589
yuusoo <yusoo@163.com>
2022-07-27 15:16:58 +0800

    first commit

---
+++ src/main/java/yys/hst/git/Gittest.java
@@ -0,0 +1,12 @@
+package yys.hst.git;
+
+/**
+ * @ Author YUUSOO
+ * @ Date 2022/7/27 15:12
+ * @ jdk:
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
